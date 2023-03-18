4d700c500f595f51c87262473a58595b1b526d37
Patrick Klitzke <invoices@kawazu-sakura-software.co.jp>
2023-03-18 17:54:55 +0900

    Improve code also with gpt

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,16 +1,96 @@
-use std::env::{args,Args};
 
-fn get_number(args: &mut Args) -> i32 {
-    return args.next().unwrap().parse::<i32>().unwrap()
+use std::env::{args, Args};
+
+struct Input {
+    number1: i32,
+    operator: char,
+    number2: i32,
 }
 
-fn calculate(num: i32, op: char, num2: i32) -> i32 {
-    match op {
-        '+' => return num + num2,
-        '-' => return num - num2,
-        '*' => return num * num2,
-        '/' => return num / num2,
-        _ => return 0,
+impl Input {
+    fn calculate(&self) -> Option<i32> {
+        match self.operator {
+            '+' => Some(self.number1 + self.number2),
+            '-' => Some(self.number1 - self.number2),
+            '*' => Some(self.number1 * self.number2),
+            '/' => if self.number2 != 0 {
+                Some(self.number1 / self.number2)
+            } else {
+                None
+            },
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_plus() {
+        let input = Input { number1: 2, operator: '+', number2: 3 };
+        assert_eq!(input.calculate(), Some(5));
+    }
+
+    #[test]
+    fn test_minus() {
+        let input = Input { number1: 2, operator: '-', number2: 3 };
+        assert_eq!(input.calculate(), Some(-1));
+    }
+
+    #[test]
+    fn test_multiply() {
+        let input = Input { number1: 2, operator: '*', number2: 3 };
+        assert_eq!(input.calculate(), Some(6));
+    }
+
+    #[test]
+    fn test_divide() {
+        let input = Input { number1: 6, operator: '/', number2: 3 };
+        assert_eq!(input.calculate(), Some(2));
+    }
+
+    #[test]
+    fn test_divide_by_zero() {
+        let input = Input { number1: 6, operator: '/', number2: 0 };
+        assert_eq!(input.calculate(), None);
+    }
+
+    #[test]
+    fn test_unsupported_operator() {
+        let input = Input { number1: 2, operator: '%', number2: 3 };
+        assert_eq!(input.calculate(), None);
+    }
+
+   #[test]
+    fn test_valid_operator() {
+        let operator = '+';
+        assert!(is_valid_operator(operator));
+    }
+
+    #[test]
+    fn test_invalid_operator() {
+        let operator = '%';
+        assert!(!is_valid_operator(operator));
+    }
+}
+
+fn get_number(args: &mut Args) -> Option<i32> {
+    args.next()?.parse::<i32>().ok()
+}
+
+
+fn is_valid_operator(op: char) -> bool {
+    matches!(op, '+' | '-' | '*' | '/')
+}
+
+fn get_operator(args: &mut Args) -> Option<char> {
+    let op = args.next()?.chars().next()?;
+    if is_valid_operator(op) {
+        Some(op)
+    } else {
+        None
     }
 }
 
@@ -18,17 +98,26 @@ fn main() {
     let mut args = args();
 
     if args.len() != 4 {
-        panic!("3 parameters are required");
+        eprintln!("3 parameters are required: number1 operator number2");
+        std::process::exit(1);
     }
 
     // discard first arg
     args.next();
 
-    let num1: i32 = get_number(&mut args);
-    let op: char = args.next().unwrap().chars().next().unwrap();
-    let num2 = get_number(&mut args);
+    let num1 = get_number(&mut args).expect("Failed to parse first number");
+    let op = get_operator(&mut args).expect("Invalid operator");
+    let num2 = get_number(&mut args).expect("Failed to parse second number");
 
-    println!("{:?}", args);
-    println!("RES {} {} {} = {}", num1, op, num2, calculate(num1, op, num2));
-    println!("Hello, world!");
+    let input = Input {
+        number1: num1,
+        operator: op,
+        number2: num2,
+    };
+
+    match input.calculate() {
+        Some(result) => println!("{} {} {} = {}", input.number1, input.operator, input.number2, result),
+        None => eprintln!("Error: Unsupported operator or invalid operation"),
+    }
 }
+
