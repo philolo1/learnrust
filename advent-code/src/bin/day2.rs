3857f5df4864e49fb6aff9698225ee6b075d591e
Patrick Klitzke <philologos14@gmail.com>
2023-05-07 00:59:53 +0900

    Finish day2

--- advent-code/src/bin/day2.rs
+++ advent-code/src/bin/day2.rs
@@ -9,10 +9,23 @@ enum Play {
     Scissors,
 }
 
+#[derive(Debug, Copy, Clone)]
+enum GameResult {
+    Lose,
+    Draw,
+    Win,
+}
+
 #[derive(Debug)]
 struct Game {
     player1: Play,
     player2: Play
+}
+
+#[derive(Debug)]
+struct NewGame {
+    player1: Play,
+    player2: GameResult
 }
 
 impl FromStr for Play {
@@ -29,6 +42,21 @@ impl FromStr for Play {
         return Ok(player1)
     }
 
+}
+
+impl FromStr for GameResult {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let res = match s {
+            "X"  => GameResult::Lose,
+            "Y" => GameResult::Draw,
+            "Z" => GameResult::Win,
+            _ => return Err(anyhow!("Invalid play: {}", s)),
+        };
+
+        return Ok(res)
+    }
 }
 
 impl Play {
@@ -57,6 +85,22 @@ impl FromStr for Game {
     }
 }
 
+impl FromStr for NewGame {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let (left, right) = s.split_once(' ').context("Needs space seperated")?;
+
+        let player1 = str::parse(left)?;
+        let player2 = str::parse(right)?;
+
+        return Ok(NewGame{
+            player1,
+            player2
+        })
+    }
+}
+
 impl Game {
     fn score(&self) -> i32 {
         let mut my_score = match (self.player1, self.player2) {
@@ -77,6 +121,25 @@ impl Game {
     }
 }
 
+impl From<NewGame> for Game {
+    fn from(value: NewGame) -> Self {
+        let player1 = value.player1;
+        let player2 = match (value.player1, value.player2) {
+            (Play::Rock, GameResult::Lose) => Play::Scissors,
+            (Play::Rock, GameResult::Draw) => Play::Rock,
+            (Play::Rock, GameResult::Win) => Play::Paper,
+            (Play::Paper, GameResult::Lose) => Play::Rock,
+            (Play::Paper, GameResult::Draw) => Play::Paper,
+            (Play::Paper, GameResult::Win) => Play::Scissors,
+            (Play::Scissors, GameResult::Lose) => Play::Paper,
+            (Play::Scissors, GameResult::Draw) => Play::Scissors,
+            (Play::Scissors, GameResult::Win) => Play::Rock,
+        };
+
+        return Game { player1, player2 };
+    }
+}
+
 fn main() -> Result<()> {
     let file_name = env::args().nth(1).context("one file is necessary")?;
     println!("file_name: {:?}", file_name);
@@ -85,6 +148,7 @@ fn main() -> Result<()> {
 
     let games: Vec<Game> = content.lines()
         .flat_map(str::parse)
+        .map(|x: NewGame| Game::from(x))
         .collect();
 
     let scores: Vec<i32> = games.iter().map(|x| x.score()).collect();
