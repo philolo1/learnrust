e8b07b05ddd98d67c2f573d1d0e3ec003f91b70a
Patrick Klitzke <philologos14@gmail.com>
2023-05-07 20:22:27 +0900

    Day 7 wip

---
+++ advent-code/src/bin/day7.rs
@@ -0,0 +1,129 @@
+use std::{fs, str::FromStr};
+use anyhow::{Error, Result, Context};
+use std::env;
+
+#[derive(Debug, Clone)]
+struct File {
+    size: usize,
+    name: String
+}
+
+impl FromStr for File {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let (size, name) = s.split_once(" ").context("needs space")?;
+        let size = str::parse(size)?;
+
+        return Ok(
+            File{
+                size,
+                name: name.to_string()
+            }
+        );
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Directory {
+    parent: Option<Box<Directory>>,
+    current_path: String,
+    directories: Vec<Directory>,
+    files: Vec<File>
+}
+
+impl Directory {
+    fn add_files(&mut self, files: &Vec<&str>) {
+        self.files = files
+            .iter()
+            .flat_map(|x| str::parse::<File>(x))
+            .collect();
+    }
+
+    fn add_dics(&mut self, dics: &Vec<&str>) {
+        let new_box = Box::new(self.clone());
+        self.directories = dics
+            .iter()
+            .map(|x| {
+                let (_, name) = x.split_once(' ').unwrap();
+                return Directory {
+                    parent: Some(new_box.clone()),
+                    current_path: String::from(name),
+                    directories: vec![],
+                    files: vec![]
+                }
+            })
+            .collect();
+    }
+}
+
+
+
+fn main() -> Result<()> {
+    let file_name = env::args().nth(1).context("one file is necessary")?;
+    println!("file_name: {:?}", file_name);
+
+    let content = fs::read_to_string(file_name)?;
+
+    let mut main_dir = Directory{
+        parent: None,
+        current_path: String::from("/"),
+        directories: vec![],
+        files: vec![]
+    };
+
+    let commands: Vec<&str> = content.split("$").filter(|x| x.len() > 0).map(|x| x.trim()).collect();
+
+    println!("Commands: {:?}", commands);
+
+    let mut current_dic = Box::new(main_dir.clone());
+
+    for command in commands {
+        let split_opt = command.split_once("\n");
+
+        if let None = split_opt {
+            println!("cd {}", command);
+            let cmd = command[2..].trim().to_string();
+            if cmd == "/" {
+                current_dic = Box::new(main_dir.clone())
+            } else if cmd == ".." {
+                current_dic = current_dic.parent.unwrap()
+            } else {
+                for dic in &current_dic.directories {
+                    println!("Hello world");
+                    if dic.current_path == cmd {
+                        println!("path found");
+                        // TODO this might make problems
+                        current_dic = Box::new(dic.clone());
+                        break;
+                    }
+                }
+            }
+        }
+
+        if let Some((_,b)) = split_opt {
+            // println!("ls {}\n {}", a, b);
+            let dics: Vec<&str> = b.lines().filter(|x| x.contains("dir")).collect();
+
+            current_dic.add_dics(&dics);
+
+            // println!("DIR {:?}", dics);
+
+            let files: Vec<&str> = b.lines().filter(|x| !x.contains("dir")).collect();
+
+            current_dic.add_files(&files);
+
+            // println!("Files {:?}", files);
+
+
+            // println!("current_dic {:?}", current_dic);
+
+        }
+    }
+
+
+    println!("main_dics {:?}", current_dic);
+
+
+    return Ok(());
+}
