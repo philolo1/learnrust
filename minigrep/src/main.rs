3193054e99b0f14445f7cbae9f15bdf6efcfd6fd
Patrick Klitzke <philologos14@gmail.com>
2023-06-28 20:27:44 +0900

    Chapter 16

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,45 +1,16 @@
 use std::env;
-use std::fs;
 use std::error::Error;
 
-struct Config {
-    query: String,
-    filename: String
-}
+use minigrep::Config;
+
 
 fn main() -> Result<(),  Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args)?;
 
-
-    return run(config);
+    return minigrep::run(config);
 }
 
-fn run(config: Config) -> Result<(),  Box<dyn Error>> {
 
-    println!("Searching for {}", config.query);
-    println!("File {}", config.filename);
 
-    let  contents = fs::read_to_string(config.filename)?;
-
-    println!("Content: {}", contents);
-    Ok(())
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-
-        if args.len() != 3 {
-            return Err("Exactly two parameters need to be provided");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {
-            query,
-            filename
-        })
-    }
-}
