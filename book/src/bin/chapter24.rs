3939752a8a25703cfaee63d53bb1580bc2308d85
Patrick Klitzke <philologos14@gmail.com>
2023-06-30 20:16:43 +0900

    Video 24

---
+++ book/src/bin/chapter24.rs
@@ -0,0 +1,25 @@
+
+#[derive(Debug)]
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    drop(c);
+
+    println!("Smart pointer created");
+}
