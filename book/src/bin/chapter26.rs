eab37ff4ada85a0402a99366798217e42253c8ce
Patrick Klitzke <philologos14@gmail.com>
2023-07-01 20:18:56 +0900

    Video 26

--- book/src/bin/chapter26.rs
+++ book/src/bin/chapter26.rs
@@ -20,7 +20,7 @@ pub struct LimitTracker<'a, T: Messenger> {
 impl<'a, T> LimitTracker<'a, T>
 where
     T: Messenger {
-        pub fn new(messenger: &T, max: uszie) -> LimitTracker<T> {
+        pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
             LimitTracker {
                 messenger,
                 value: 0,
@@ -30,9 +30,51 @@ where
 
         pub fn set_value(&mut self, value: usize) {
             self.value = value;
-            todo!();
+            let percentage_of_max = self.value as f64 / self.max as f64;
+
+            if percentage_of_max >= 1.0 {
+                self.messenger.send("Error: You are over the limit");
+            } else if percentage_of_max >= 0.9 {
+                self.messenger.send("Urgent warning, you are close to the limit");
+            } else if percentage_of_max >= 0.75 {
+                self.messenger.send("Warning, you have used more then 75%");
+            }
         }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_send_an_over_75() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+        limit_tracker.set_value(80);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+
 }
 
 fn main() {
 }
+
