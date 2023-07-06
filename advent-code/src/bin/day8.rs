486e9d27f6a49e7899e818acec40bbb4466e3e35
Patrick Klitzke <philologos14@gmail.com>
2023-07-06 11:35:58 +0300

    Day 8 part 1

---
+++ advent-code/src/bin/day8.rs
@@ -0,0 +1,103 @@
+use std::{fs, str::FromStr};
+use anyhow::{anyhow, Error, Result, Context};
+use std::env;
+use std::collections::HashMap;
+
+
+#[derive(Debug)]
+struct Tree {
+    value: u32,
+    is_visible: bool,
+}
+
+impl  Tree {
+
+    fn mark_visible(&mut self) {
+        self.is_visible = true;
+    }
+
+    fn from_char(s: &char) -> Result<Self> {
+        let value: u32 = format!("{}", s).parse()?;
+        return Ok(Tree {
+            value,
+            is_visible: false
+        })
+    }
+}
+
+
+
+fn main() ->  Result<()> {
+    let file_name = env::args().nth(1).context("One file is necessary")?;
+    let content = fs::read_to_string(file_name)?;
+
+    let mut tree_arr: Vec<Vec<Tree>> = content.lines().map(|x| {
+        let el: Vec<char> = x.chars().collect();
+        let tree : Vec<Tree> = el.iter()
+            .flat_map(|x: &char| Tree::from_char(x).context("Test"))
+            .collect();
+        return tree;
+    }).collect();
+
+
+
+    let col_size = tree_arr.len();
+    let row_size = tree_arr[0].len();
+
+    for a in 0..col_size {
+        let mut max = tree_arr[a][0].value;
+        for b in 0..row_size {
+            if tree_arr[a][b].value > max {
+                tree_arr[a][b].mark_visible();
+                max = tree_arr[a][b].value;
+            }
+        }
+    }
+
+    for a in 0..col_size {
+        let mut max = tree_arr[a][row_size - 1].value;
+        for b in (0..row_size).rev() {
+            if tree_arr[a][b].value > max {
+                tree_arr[a][b].mark_visible();
+                max = tree_arr[a][b].value;
+            }
+        }
+    }
+
+    for b in 0..row_size {
+        let mut max = tree_arr[0][b].value;
+        for a in 0..col_size {
+            if tree_arr[a][b].value > max {
+                tree_arr[a][b].mark_visible();
+                max = tree_arr[a][b].value;
+            }
+        }
+    }
+
+    for b in 0..row_size {
+        let mut max = tree_arr[col_size - 1][b].value;
+        for a in (0..col_size).rev() {
+            if tree_arr[a][b].value > max {
+                tree_arr[a][b].mark_visible();
+                max = tree_arr[a][b].value;
+            }
+        }
+    }
+
+    for b in 0..row_size {
+        tree_arr[col_size - 1][b].mark_visible();
+        tree_arr[0][b].mark_visible();
+    }
+
+    for a in 0..col_size {
+        tree_arr[a][0].mark_visible();
+        tree_arr[a][row_size - 1].mark_visible();
+    }
+
+    let sum :u32 = tree_arr.iter().flatten().filter(|x| x.is_visible).map(|x| 1).sum();
+
+    println!("SUM: {}", sum);
+
+
+    Ok(())
+}
