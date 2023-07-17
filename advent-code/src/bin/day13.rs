f094f4827d3d9f416d12ccea89dc95eab814ad98
Patrick Klitzke <philologos14@gmail.com>
2023-07-17 09:58:45 +0300

    day 13 wip

--- advent-code/src/bin/day13.rs
+++ advent-code/src/bin/day13.rs
@@ -1,17 +1,54 @@
-use std::str::FromStr;
+use std::cmp::max;
+use std::rc::Rc;
 use std::{fs};
-use anyhow::{Error, Result, Context};
+use anyhow::{Result, Context};
 use std::env;
-use std::collections::VecDeque;
-
-
-use std::collections::HashMap;
 
 
 #[derive(Debug)]
 enum List {
-    MyList(Vec<Box<List>>),
+    MyList(Vec<Rc<List>>),
     El(i32)
+}
+
+fn compare_list(left: Rc<List>, right: Rc<List>) -> bool {
+
+
+
+    let left_list_reference = Rc::clone(&left);
+    let left_list: &List = &*left_list_reference;
+
+    let right_list_reference = Rc::clone(&right);
+    let right_list: &List = &*right_list_reference;
+
+    return match (*left_list, *right_list) {
+        (List::MyList(v1), List::MyList(v2)) => {
+            let len = max(v1.len(), v2.len());
+
+            for i in 0..len {
+                if compare_list(v1[i], v2[i]) == false {
+                    return false;
+                }
+            }
+
+            if v1.len() <= v2.len() {
+                return true;
+            }
+
+            return false;
+        },
+        (List::MyList(_), List::El(_)) => {
+            let new_right = right;
+            let right_el = Rc::new(List::MyList(vec![new_right]));
+            return compare_list(left, right_el);
+        },
+        (List::El(_), List::MyList(e2)) => {
+            let new_left = left;
+            let left_el = Rc::new(List::MyList(vec![new_left]));
+            return compare_list(left_el, right);
+        },
+        (List::El(e1), List::El(e2)) => e1 <= e2,
+    }
 }
 
 fn create_list(s: &str, pos: &mut usize) -> List {
@@ -34,7 +71,7 @@ fn create_list(s: &str, pos: &mut usize) -> List {
                 break;
             }
 
-            list.push(Box::new(item));
+            list.push(Rc::new(item));
 
             let ch =  s.chars().nth(*pos).unwrap() ;
 
@@ -78,6 +115,15 @@ fn main() ->  Result<()> {
     let res: Vec<&str> = content.split("\n\n").collect();
 
     println!("Res: {:?}", res);
+
+    for item in res {
+        let (left, right) = item.split_once('\n').unwrap();
+        println!("Left:{left}\nRight:{right}\n");
+        let mut pos = 0;
+        let left = create_list(left, &mut pos);
+        let mut pos = 0;
+        let right = create_list(right, &mut pos);
+    }
 
     // let mut pos = 0;
 
