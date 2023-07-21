d29147f71b6a2e68b30715ed5eadb3e3762bf8e6
Patrick Klitzke <philologos14@gmail.com>
2023-07-22 04:08:20 +0900

    first try

--- advent-code/src/bin/day13.rs
+++ advent-code/src/bin/day13.rs
@@ -1,4 +1,5 @@
-use std::cmp::max;
+use std::cell::RefCell;
+use std::cmp::min;
 use std::rc::Rc;
 use std::{fs};
 use anyhow::{Result, Context};
@@ -7,52 +8,54 @@ use std::env;
 
 #[derive(Debug)]
 enum List {
-    MyList(Vec<Rc<List>>),
+    MyList(Vec<Rc<RefCell<List>>>),
     El(i32)
 }
 
-fn compare_list(left: Rc<List>, right: Rc<List>) -> bool {
+fn compare_list(left: &Rc<RefCell<List>>, right: &Rc<RefCell<List>>) -> bool {
 
+    let left_el = left.borrow();
+    let left_el = &*left_el;
+    let right_el = right.borrow();
+    let right_el = &*right_el;
 
-
-    let left_list_reference = Rc::clone(&left);
-    let left_list: &List = &*left_list_reference;
-
-    let right_list_reference = Rc::clone(&right);
-    let right_list: &List = &*right_list_reference;
-
-    return match (*left_list, *right_list) {
+    match(left_el, right_el) {
         (List::MyList(v1), List::MyList(v2)) => {
-            let len = max(v1.len(), v2.len());
+            let len = min(v1.len(), v2.len());
 
             for i in 0..len {
-                if compare_list(v1[i], v2[i]) == false {
+                let w1 = &v1[i];
+                let w2 = &v2[i];
+                if compare_list(w1,w2) == false {
                     return false;
                 }
             }
 
+            println!("HERE? {} {}", v1.len(), v2.len());
             if v1.len() <= v2.len() {
                 return true;
             }
 
             return false;
         },
+        (List::El(e1), List::El(e2)) => {
+            println!("Compare {e1} to {e2}");
+            return e1 <= e2
+        },
         (List::MyList(_), List::El(_)) => {
-            let new_right = right;
-            let right_el = Rc::new(List::MyList(vec![new_right]));
-            return compare_list(left, right_el);
+            let arr = vec![right.clone()];
+            let new_right = &Rc::new(RefCell::new(List::MyList(arr)));
+            return compare_list(left, new_right);
         },
-        (List::El(_), List::MyList(e2)) => {
-            let new_left = left;
-            let left_el = Rc::new(List::MyList(vec![new_left]));
-            return compare_list(left_el, right);
-        },
-        (List::El(e1), List::El(e2)) => e1 <= e2,
+        (List::El(_), List::MyList(_)) => {
+            let arr = vec![left.clone()];
+            let new_left = &Rc::new(RefCell::new(List::MyList(arr)));
+            return compare_list(new_left, right);
+        }
     }
 }
 
 fn create_list(s: &str, pos: &mut usize) -> List {
-    // dbg!(*pos);
     if *pos >= s.len() {
         return List::MyList(vec![]);
     }
@@ -71,7 +74,7 @@ fn create_list(s: &str, pos: &mut usize) -> List {
                 break;
             }
 
-            list.push(Rc::new(item));
+            list.push(Rc::new(RefCell::new(item)));
 
             let ch =  s.chars().nth(*pos).unwrap() ;
 
@@ -112,18 +115,30 @@ fn main() ->  Result<()> {
     let file_name = env::args().nth(1).context("One file is necessary")?;
     let content = fs::read_to_string(file_name)?;
 
-    let res: Vec<&str> = content.split("\n\n").collect();
+    let res: Vec<&str> = content.split("\n\n").nth(1).into_iter().collect();
 
     println!("Res: {:?}", res);
 
+    let mut index = 0;
+    let mut sum = 0;
     for item in res {
+        index+=1;
         let (left, right) = item.split_once('\n').unwrap();
-        println!("Left:{left}\nRight:{right}\n");
+        println!("Index:{index}\nLeft:{left}\nRight:{right}\n");
         let mut pos = 0;
         let left = create_list(left, &mut pos);
         let mut pos = 0;
         let right = create_list(right, &mut pos);
+
+        if compare_list(&Rc::new(RefCell::new(left)), &Rc::new(RefCell::new(right))) {
+            println!("TRUE");
+            sum += index;
+        } else {
+            println!("FALSE");
+        }
     }
+
+    println!("SUM: {}", sum);
 
     // let mut pos = 0;
 
