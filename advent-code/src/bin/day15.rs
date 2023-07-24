fa0328713e832ed58c310c75f0477a824defab97
Patrick Klitzke <philologos14@gmail.com>
2023-07-25 00:58:20 +0900

    day 15 part 1

--- advent-code/src/bin/day15.rs
+++ advent-code/src/bin/day15.rs
@@ -6,7 +6,17 @@ use regex::Regex;
 
 use std::collections::HashMap;
 
-type Pair<'a> = (&'a str, &'a str);
+type Pair = (i32, i32);
+
+fn dist(p1: &Pair, p2: &Pair) -> i32 {
+   return (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs();
+}
+
+struct Sensor {
+    pos_x: i32,
+    pos_y: i32,
+    dist: i32
+}
 
 fn main() ->  Result<()> {
     println!("Day 11");
@@ -17,16 +27,48 @@ fn main() ->  Result<()> {
 
     // dbg!(lines);
 
+    let mut beacon_map = HashMap::new();
+
 
 
     // dbg!(&content);
     let re = Regex::new(r"Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)").unwrap();
 
+    let mut sensors: Vec<Sensor> = vec![];
+
     for c in re.captures_iter(&content) {
         let (_,arr) = c.extract::<4>();
         let arr = arr.map(|e| e.parse::<i32>().unwrap());
-        dbg!(&arr);
+        let [x,y,x2,y2] = arr;
+        beacon_map.insert((x2,y2), true);
+
+        let beacon_dist = dist(&(x,y),&(x2,y2));
+
+        sensors.push(Sensor {pos_x: x, pos_y: y, dist: beacon_dist});
     }
+
+    let row = 2_000_000;
+    let mut counter = 0;
+
+    'outer: for x in -10_00_000..10_000_000 {
+       if let Some(_) = beacon_map.get(&(x, row)) {
+           continue;
+       }
+
+       for s in &sensors {
+           let d = dist(&(s.pos_x, s.pos_y), &(x,row));
+
+           if d <= s.dist {
+               counter += 1;
+               continue 'outer;
+           }
+
+       }
+    }
+
+    dbg!(counter);
+
+
 
 
 
