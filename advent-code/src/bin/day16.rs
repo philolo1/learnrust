32c8fa9d2ed2554be36d96ca376aa0c0518b2fc5
Patrick Klitzke <philologos14@gmail.com>
2023-07-27 22:21:07 +0900

    day 16 part 1

--- advent-code/src/bin/day16.rs
+++ advent-code/src/bin/day16.rs
@@ -1,6 +1,6 @@
-use std::cmp::{min, max};
+use std::cmp::{max};
 use std::rc::Rc;
-use std::{fs};
+use std::fs;
 use anyhow::{Result, Context};
 use std::env;
 use regex::Regex;
@@ -29,6 +29,7 @@ impl<'a> Node<'a> {
 
 fn main() ->  Result<()> {
     println!("Day 11");
+
     let file_name = env::args().nth(1).context("One file is necessary")?;
     let content = fs::read_to_string(file_name)?;
 
@@ -39,7 +40,7 @@ fn main() ->  Result<()> {
 
 //     todo!();
     let mut node_map = HashMap::new();
-    let mut nodes = vec![];
+    let mut nodes: Vec<Rc<Node<'_>>> = vec![];
      for c in re.captures_iter(&content) {
          let (_,arr) = c.extract::<3>();
          dbg!(arr);
@@ -63,15 +64,18 @@ fn main() ->  Result<()> {
      dbg!(al);
 
      let mut non_zero_nodes : Vec<&str>= nodes.iter().filter(|node| node.rate > 0).map(|node| node.name).collect();
+     let mut non_zero_rates : Vec<i32> = vec![];
      non_zero_nodes.insert(0, "AA");
      dbg!(&non_zero_nodes);
-     let mut dist_map = HashMap::new();
+     let mut dist_map: HashMap<&&str, HashMap<&str, i32>> = HashMap::new();
 
      for node in &non_zero_nodes {
          let mut node_hash_map: HashMap<&str, i32> = HashMap::new();
 
          let mut queue = VecDeque::new();
          queue.push_back((*node, 0));
+
+         non_zero_rates.push(node_map.get(*node).unwrap().rate);
 
          while let Some((node_name, dist)) = queue.pop_front() {
              if None == node_hash_map.get(node_name) {
@@ -94,11 +98,89 @@ fn main() ->  Result<()> {
      }
 
 
+    println!("SEARCH");
+    let mut max_map = HashMap::new();
+    let res = search(
+        0,
+        30,
+        0,
+        &dist_map,
+        &non_zero_nodes,
+        &mut max_map,
+        &non_zero_rates
+    );
+
+    dbg!(res);
 
 
     Ok(())
 }
 
-fn search(current_pos: i32, minutes_left: i32, opened_pos: i32) {
+fn search(
+    current_pos: i32,
+    minutes_left: i32,
+    opened_pos: u32,
+    dist: &HashMap<&&str, HashMap<&str, i32>>,
+    non_zero_nodes: &Vec<&str>,
+    max_map: &mut HashMap<(i32,i32,u32), i32>,
+    non_zero_rates: &Vec<i32>,
+    ) -> i32 {
+
+
+    // check if already in the hashmap
+    if let Some(val) = max_map.get(&(current_pos,minutes_left,opened_pos)) {
+        return *val;
+    }
+
+
+    // no time left
+    if minutes_left == 0 {
+        return 0;
+    }
+
+    // calculate current rate
+    let mut current_rate = 0;
+    for i in 0..non_zero_rates.len() {
+        if (opened_pos >> i) & 1 == 1 {
+            current_rate += non_zero_rates[i];
+        }
+    }
+
+
+    println!("Current pos: {current_pos} min: {minutes_left} current_rate:{current_rate}");
+
+    // do nothing
+    let mut res = minutes_left * current_rate;
+
+    // check if can open vault
+    if (opened_pos >> current_pos) & 1 == 0 {
+        let new_open_pos = opened_pos | (1 << current_pos);
+        println!("{new_open_pos} {} {}", current_pos, 1 << current_pos);
+        let open_case = current_rate + search(current_pos, minutes_left - 1, new_open_pos, dist, non_zero_nodes, max_map, non_zero_rates);
+        res = max(res, open_case);
+    }
+
+    // go to somewhere else
+    for i in 0..non_zero_nodes.len() {
+        if i == current_pos as usize {
+            continue
+        }
+        let dist_map = dist.get(&non_zero_nodes[current_pos as usize]).unwrap();
+        let go_dist = *dist_map.get(&non_zero_nodes[i]).unwrap();
+
+        if go_dist > minutes_left {
+            continue
+        }
+
+        let go_case = go_dist * current_rate + search(i as i32, minutes_left - go_dist, opened_pos, dist, non_zero_nodes, max_map, non_zero_rates);
+        res = max(res, go_case);
+    }
+
+
+
+    max_map.insert((current_pos,minutes_left,opened_pos), res);
+
+    return res;
+
 }
 
